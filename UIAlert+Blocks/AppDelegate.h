a6191b707cf88036475a68ae1b48555e9337a204
Bogdan Stasjuk <Bogdan.Stasjuk@gmail.com>
2014-03-22 22:07:51 +0200

    Initial Commit

---
+++ UIAlert+Blocks/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UIAlert+Blocks
+//
+//  Created by Bogdan Stasjuk on 3/22/14.
+//  Copyright (c) 2014 Bogdan Stasjuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UIAlert+Blocks/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UIAlert+Blocks
+//
+//  Created by Bogdan Stasjuk on 3/22/14.
+//  Copyright (c) 2014 Bogdan Stasjuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
